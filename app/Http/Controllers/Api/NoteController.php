1eb9754eee62189450a9981338d112dbedb9740c
zurctrebla <albertcruz@terra.com.br>
2022-03-24 15:29:32 -0300

    Notes api documentation

--- app/Http/Controllers/Api/NoteController.php
+++ app/Http/Controllers/Api/NoteController.php
@@ -18,10 +18,34 @@ class NoteController extends Controller
     }
 
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    * @OA\Get(
+    *      path="/schedules/{scheduling}/notes",
+    *      operationId="getNotesList",
+    *      tags={"Notes"},
+    *      summary="Get list of notes",
+    *      description="Returns list of notes",
+    *      @OA\Parameter(
+    *          name="scheduling",
+    *          description="scheduling",
+    *          required=true,
+    *          in="path",
+    *          @OA\Schema(type="string"),
+    *          @OA\Examples(example="uuid", value="0006faf6-7a61-426c-9034-579f2cfcfa83", summary="An UUID value."),
+    *      ),
+    *      @OA\Response(
+    *          response=200,
+    *          description="Successful operation"
+    *       ),
+    *      @OA\Response(
+    *          response=401,
+    *          description="Unauthenticated",
+    *      ),
+    *      @OA\Response(
+    *          response=403,
+    *          description="Forbidden"
+    *      )
+    *     )
+    */
     public function index($scheduling)
     {
         $notes = $this->noteService->getNotesByScheduling($scheduling);
@@ -30,11 +54,49 @@ class NoteController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\StoreUpdateNote $request
-     * @return \Illuminate\Http\Response
-     */
+    * @OA\Post(
+    *      path="/schedules/{scheduling}/notes/",
+    *      operationId="storeNotes",
+    *      tags={"Notes"},
+    *      summary="Store new notes",
+    *      description="Returns notes data",
+    *      @OA\Parameter(
+    *          name="scheduling",
+    *          description="Scheduling",
+    *          required=true,
+    *          in="path",
+    *          @OA\Schema(type="string"),
+    *          @OA\Examples(example="uuid", value="0006faf6-7a61-426c-9034-579f2cfcfa83", summary="An UUID value."),
+    *      ),
+    *      @OA\RequestBody(
+    *          required=true,
+    *          @OA\JsonContent(
+    *              type="object",
+    *              @OA\Property(property="scheduling", type="string"),
+    *              @OA\Property(property="note", type="string")
+    *          )
+    *      ),
+    *      @OA\Response(
+    *          response=201,
+    *          description="Successful operation",
+    *          @OA\JsonContent(
+    *
+    *          )
+    *       ),
+    *      @OA\Response(
+    *          response=400,
+    *          description="Bad Request"
+    *      ),
+    *      @OA\Response(
+    *          response=401,
+    *          description="Unauthenticated",
+    *      ),
+    *      @OA\Response(
+    *          response=403,
+    *          description="Forbidden"
+    *      )
+    * )
+    */
     public function store(StoreUpdateNote $request, $scheduling)
     {
         $scheduling = $this->noteService->createNewNote($request->validated());
@@ -43,11 +105,42 @@ class NoteController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    * @OA\Get(
+    *      path="/schedules/{scheduling}/notes/{note}",
+    *      operationId="getNoteList",
+    *      tags={"Notes"},
+    *      summary="Get notes",
+    *      description="Returns list of notes",
+    *       @OA\Parameter(
+    *         description="Parameter with multiple examples",
+    *         in="path",
+    *         name="scheduling",
+    *         required=true,
+    *         @OA\Schema(type="string"),
+    *         @OA\Examples(example="uuid", value="0006faf6-7a61-426c-9034-579f2cfcfa83", summary="An UUID value."),
+    *     ),
+    *       @OA\Parameter(
+    *         description="Parameter with multiple examples",
+    *         in="path",
+    *         name="note",
+    *         required=true,
+    *         @OA\Schema(type="string"),
+    *         @OA\Examples(example="uuid", value="0006faf6-7a61-426c-9034-579f2cfcfa83", summary="An UUID value."),
+    *     ),
+    *      @OA\Response(
+    *          response=200,
+    *          description="Successful operation"
+    *       ),
+    *      @OA\Response(
+    *          response=401,
+    *          description="Unauthenticated",
+    *      ),
+    *      @OA\Response(
+    *          response=403,
+    *          description="Forbidden"
+    *      )
+    *     )
+    */
     public function show($scheduling, $identify)
     {
         $scheduling = $this->noteService->getNoteByscheduling($scheduling, $identify);
@@ -56,12 +149,62 @@ class NoteController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\StoreUpdateNote $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    * @OA\Put(
+    *      path="/schedules/{scheduling}/notes/{note}",
+    *      operationId="updateNote",
+    *      tags={"Notes"},
+    *      summary="Update existing note",
+    *      description="Returns updated note data",
+    *      @OA\Parameter(
+    *          description="Parameter with multiple examples",
+    *          in="path",
+    *          name="scheduling",
+    *          required=true,
+    *          @OA\Schema(type="string"),
+    *          @OA\Examples(example="uuid", value="0006faf6-7a61-426c-9034-579f2cfcfa83", summary="An UUID value."),
+    *      ),
+    *      @OA\Parameter(
+    *          description="Parameter with multiple examples",
+    *          in="path",
+    *          name="note",
+    *          required=true,
+    *          @OA\Schema(type="string"),
+    *          @OA\Examples(example="uuid", value="0006faf6-7a61-426c-9034-579f2cfcfa83", summary="An UUID value."),
+    *      ),
+    *      @OA\RequestBody(
+    *          required=true,
+    *          @OA\JsonContent(
+    *              @OA\Property(property="scheduling", type="string"),
+    *              @OA\Property(property="note", type="string")
+    *          )
+    *      ),
+    *      @OA\Response(
+    *          response=202,
+    *          description="Successful operation",
+    *          @OA\JsonContent(
+    *              @OA\Property(property="identify", type="string"),
+    *              @OA\Property(property="scheduling", type="string"),
+    *              @OA\Property(property="obs", type="string")
+    *          )
+    *       ),
+    *      @OA\Response(
+    *          response=400,
+    *          description="Bad Request"
+    *      ),
+    *      @OA\Response(
+    *          response=401,
+    *          description="Unauthenticated",
+    *      ),
+    *      @OA\Response(
+    *          response=403,
+    *          description="Forbidden"
+    *      ),
+    *      @OA\Response(
+    *          response=404,
+    *          description="Resource Not Found"
+    *      )
+    * )
+    */
     public function update(StoreUpdateNote $request, $scheduling,  $identify)
     {
         $this->noteService->updateNote($identify, $request->validated());
@@ -70,11 +213,47 @@ class NoteController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    * @OA\Delete(
+    *      path="/schedules/{scheduling}/notes/{note}",
+    *      operationId="deleteNote",
+    *      tags={"Notes"},
+    *      summary="Delete existing note",
+    *      description="Deletes a record and returns no content",
+    *       @OA\Parameter(
+    *           description="Parameter with mutliple examples",
+    *           in="path",
+    *           name="scheduling",
+    *           required=true,
+    *           @OA\Schema(type="string"),
+    *           @OA\Examples(example="uuid", value="0006faf6-7a61-426c-9034-579f2cfcfa83", summary="An UUID value.")
+    *       ),
+    *       @OA\Parameter(
+    *           description="Parameter with multiple examples",
+    *           in="path",
+    *           name="note",
+    *           required=true,
+    *           @OA\Schema(type="string"),
+    *           @OA\Examples(example="uuid", value="0006faf6-7a61-426c-9034-579f2cfcfa83", summary="An UUID value.")
+    *      ),
+    *      @OA\Response(
+    *          response=204,
+    *          description="Successful operation",
+    *          @OA\JsonContent()
+    *      ),
+    *      @OA\Response(
+    *          response=401,
+    *          description="Unauthenticated",
+    *      ),
+    *      @OA\Response(
+    *          response=403,
+    *          description="Forbidden"
+    *      ),
+    *      @OA\Response(
+    *          response=404,
+    *          description="Resource Not Found"
+    *      )
+    * )
+    */
     public function destroy($scheduling, $identify)
     {
         $this->noteService->deleteNote($identify);
